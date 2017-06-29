82208456729091f218c9f4edac675fd87677aa54
Abergard <littlinker@gmail.com>
2017-06-29 16:48:54 +0200

    add yaml-mode

---
+++ private/packages/bears-yaml.el
@@ -0,0 +1,12 @@
+;;; bears-yaml.el --- local styles
+;;; Commentary:
+
+;;; Code:
+
+(use-package yaml-mode)
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+
+;;; bears-yaml.el ends here
