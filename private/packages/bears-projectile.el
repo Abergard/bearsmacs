fa0d5a796fe59e09649395c1946b1e9e5506eb76
Abergard <littlinker@gmail.com>
2018-07-30 22:15:37 +0200

    add <f5> <f7> keybinds for projectile

--- private/packages/bears-projectile.el
+++ private/packages/bears-projectile.el
@@ -1,12 +1,20 @@
 ;;; bears-projectile.el --- local styles
 ;;; Commentary:
-
+;;; WIP : better integration with *compilation* buffer
+;;;       - show/hide *compilation* buffer to *auto* hide if not needed
 ;;; Code:
 
 (use-package projectile
   :config
   (projectile-mode 1)
+  ;; (defadvice projectile--run-project-cmd (before bears-projectile-compilation activate)
+  ;;   (toggle-maximize-buffer))
+  ;; (defadvice projectile--run-project-cmd (after bears-projectile-compilation-after activate)
+  ;;   (toggle-maximize-buffer))
   :diminish projectile-mode
+  :bind
+  ("<f5>" . projectile-compile-project)
+  ("<f7>" . projectile-run-project)
   )
 
 ;; Local Variables:
