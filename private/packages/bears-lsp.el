87324fad0559194399a3c6db82bfde7049c52939
Abergard <littlinker@gmail.com>
2019-08-30 07:38:48 +0200

    introduce lsp package

---
+++ private/packages/bears-lsp.el
@@ -0,0 +1,35 @@
+;;; bears-lsp.el --- local styles
+;;; Commentary:
+
+;;; Code:
+
+(use-package lsp-mode
+  :ensure t
+  :hook (prog-mode . lsp)
+  :config
+
+  (setq lsp-prefer-flymake nil)
+
+  (use-package lsp-ui
+    :ensure t
+    :config
+    (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+    (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+    )
+
+  (use-package company-lsp
+    :after company
+    :ensure t
+    )
+
+  (use-package lsp-treemacs
+    :after treemacs
+    :ensure t
+    )
+  )
+
+;; Local Variables:
+;; byte-compile-warnings: ()
+;; End:
+
+;;; bears-lsp.el ends here
